3319dabbb07d8e593ad9d6cd3f47f38d3096112d
Luis F Rocha <luisfrocha@icloud.com>
2021-02-05 03:25:33 -0600

    - Fixed path for css file

--- src/plugins/reveal.js
+++ src/plugins/reveal.js
@@ -3,7 +3,7 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import RevealMath from 'reveal.js/plugin/math/math.esm.js';
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
-import '../assets/css/main.scss';
+import '../assets/css/main.css';
 // import '../assets/css/slides.css';
 // import '../assets/css/deck.css';
 
